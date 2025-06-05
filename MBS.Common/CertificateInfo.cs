eebbc89579b336b545e146f3d0c355dc1d025551
bentsedev <poch_ben@hotmail.com>
2025-06-05 12:50:18 -0600

    Add files via upload

---
+++ MBS.Common/CertificateInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBS.Common
+{
+    public class CertificateInfo
+    {
+        public string Subject { get; set; }
+
+        public string Issuer { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+
+        public DateTime ValidTo { get; set; }
+    }
+}
